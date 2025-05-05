fcfadd2094b05c66a91ed77881e83e0e338b0533
Liam Colburn <colburnw@oregonstate.edu>
2025-05-05 13:24:11 -0700

    Change nav color

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -6,11 +6,21 @@ import { BuiltItLogo } from './BuiltItLogo';
 // This is our navigation bar at the top of the website.
 export default function Nav() {
   return (
-    <AppBar position="static" color="default" elevation={1}>
-      <Toolbar>
-        <Box sx={{ flexGrow: 1 }}>
+    <AppBar position="static" color="default" elevation={1} sx={{ backgroundColor: '#e8f5e9' }}>
+      <Toolbar sx={{ minHeight: '64px !important' }}>
+        <Box sx={{ flexGrow: 1, display: 'flex', alignItems: 'center' }}>
           <Link href="/" style={{ textDecoration: 'none' }}>
-            <BuiltItLogo width={125} height={50} />
+            <Box
+              sx={{
+                transform: 'scale(1.3)',       // Scale the logo up
+                transformOrigin: 'left center', // Anchor the scale to the left
+                height: '70px',                 // Restrict height so toolbar stays stable
+                display: 'flex',
+                alignItems: 'center',
+              }}
+            >
+              <BuiltItLogo height={75} width={175}/>
+            </Box>
           </Link>
         </Box>
       </Toolbar>
