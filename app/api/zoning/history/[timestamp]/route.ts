9ebff32d7d928d0f9bfcde3185ffa349b6847a85
Liam Colburn <colburnw@oregonstate.edu>
2025-05-23 10:30:30 -0700

    Create endpoint to get single zoning report data

---
+++ app/api/zoning/history/[timestamp]/route.ts
@@ -0,0 +1,33 @@
+import { NextResponse, NextRequest } from "next/server";
+import { GetCommand } from "@aws-sdk/lib-dynamodb";
+import { docClient } from "@/app/lib/dynamodb";
+import { auth0 } from "@/lib/auth0";
+
+export async function GET(params: {timestamp: string}) {
+    try {
+        const { user } = await auth0.getSession();
+        const userId = user.sub;
+        const timestamp = params.timestamp;
+
+        if (!timestamp) {
+            return Response.json({error: "No timestamp in request"})
+        }
+
+        const response = await docClient.send(
+            new GetCommand({
+                TableName: process.env.AWS_DATABASE_TABLE,
+                Key: {
+                    userId: userId,
+                    timestamp: timestamp
+                }
+            })
+        );
+        if (!response.Item) {
+            return NextResponse.json({ error: "Item not found" }, { status: 404 });
+        }
+
+        return NextResponse.json(response);
+    } catch (e: any) {
+        return NextResponse.json({error: e.message}, {status: 500});
+    }
+}
